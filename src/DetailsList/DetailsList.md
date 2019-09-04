304f03ec3a440e5c5917c5701f230f3b50ad0fa5
Savinskiy, Anton <asavinskiy@paypal.com>
2019-09-04 15:12:38 -0700

    fix(DetailsList): bringing uxpID

--- src/DetailsList/DetailsList.md
+++ src/DetailsList/DetailsList.md
@@ -2,6 +2,7 @@
   
 ```jsx
 <DetailsList 
+  uxpId="DetailsList1"
   columns="Aa|Bb|Cc"
   items="A-1 | B-1 | C-1 || A-2 | B-2 | C-2"
   isResizable />
--- src/DetailsList/presets/0-default.jsx
+++ src/DetailsList/presets/0-default.jsx
@@ -3,6 +3,7 @@ import DetailsList from '../DetailsList';
 
 export default (
   <DetailsList
+    uxpId="DetailsList1"
     columns="Aa|Bb|Cc"
     items="A-1 | B-1 | C-1 || A-2 | B-2 | C-2"
     isResizable />
