9b501cd020027d9eae59681b081e871f322c0d31
Savinskiy, Anton <asavinskiy@paypal.com>
2019-09-04 15:52:15 -0700

    chore(DetailsList): trying templating in items defaults

--- src/DetailsList/DetailsList.md
+++ src/DetailsList/DetailsList.md
@@ -3,8 +3,12 @@
 ```jsx
 <DetailsList 
   uxpId="DetailsList1"
-  columns="Aa|Bb|Cc"
-  items="A-1 | B-1 | C-1 || A-2 | B-2 | C-2"
+  columns="Aa | Bb | Cc"
+  items={
+   `A-1 | B-1 | C-1 
+    A-2 | B-2 | C-2
+    A-3 | B-3 | C-3`
+  }
   isResizable />
 ```
   
--- src/DetailsList/presets/0-default.jsx
+++ src/DetailsList/presets/0-default.jsx
@@ -4,7 +4,11 @@ import DetailsList from '../DetailsList';
 export default (
   <DetailsList
     uxpId="DetailsList1"
-    columns="Aa|Bb|Cc"
-    items="A-1 | B-1 | C-1 || A-2 | B-2 | C-2"
+    columns="Aa | Bb | Cc"
+    items={
+      `A-1 | B-1 | C-1 
+    A-2 | B-2 | C-2
+    A-3 | B-3 | C-3`
+    }
     isResizable />
 )
