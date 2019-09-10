6812c8aa7eaea3dc6dfed6999bba4e75318e9905
Savinskiy, Anton <asavinskiy@paypal.com>
2019-09-10 13:30:06 -0700

    chore(Nav): working with mock list and compiling error is fixed

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,15 +1,78 @@
 import * as React from 'react';
-import {Nav as FNav} from 'office-ui-fabric-react';
 import * as PropTypes from 'prop-types';
+import { Nav as FNav } from 'office-ui-fabric-react';
 
-function Nav(props) {
-   return (
-      <FNav {...props}>{props.children}</FNav>
-  );
+
+class Nav extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedIndex: this.props.selectedIndex | 1
+        }
+    }
+
+    getStyles() {
+        return {
+            root: {
+                width: this.props.width,
+            },
+            navItem: {
+                backgroundColor: '#fff'
+            }
+        }
+    }
+
+    getGroups() {
+        return [{
+            name: 'Extension Overview',
+            icon: 'Home',
+            key: 'overview',
+            onClick: this.onMenuClick.bind(this)
+        },
+        {
+            name: 'Extension Nav 1',
+            icon: 'OpenFolderHorizontal',
+            key: 'nav1',
+            onClick: this.onMenuClick.bind(this)
+        },]
+    }
+
+
+    onMenuClick(event, element) {
+        event.preventDefault();
+        this.setState({
+            selectedIndex: this.getGroups().findIndex(link => link.key === element.key) + 1
+        }, () => {
+            return element.name
+        })
+    }
+
+    render() {
+        const links = this.getGroups()
+        return (
+            <FNav
+                selectedKey={links[this.state.selectedIndex - 1].key}
+                expandButtonAriaLabel="Expand or collapse"
+                selectedAriaLabel="Selected"
+                styles={this.getStyles()}
+                groups={[{ links }]}
+                width={300} />
+        )
+    }
 }
 
 Nav.propTypes = {
-    children: PropTypes.node,
+    /** Max width for menu */
+    width: PropTypes.number,
+
+    /** Which element number should be selected from 1 to n */
+    selectedIndex: PropTypes.number,
 };
+
+Nav.defaultProps = {
+    width: 300,
+    selectedIndex: 1
+};
+
 
 export { Nav as default };
--- src/Nav/presets/0-default.jsx
+++ src/Nav/presets/0-default.jsx
@@ -1,66 +1,8 @@
 import * as React from 'react';
-import { Nav as FNav } from 'office-ui-fabric-react';
+import Nav from '../Nav';
 
-
-class Nav extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    getStyles() {
-        return {
-            root: {
-                width: this.props.width,
-            },
-            navItem: {
-                backgroundColor: '#fff'
-            }
-        }
-    }
-
-    getGroups() {
-        return [{
-            name: 'Extension Overview',
-            url: 'dashboard',
-            icon: 'Home',
-            key: 'overview',
-            // onClick: this.onMenuClick.bind(this)
-        },
-        {
-            name: 'Extension Nav 1',
-            url: 'nav1',
-            icon: 'OpenFolderHorizontal',
-            key: 'nav1',
-            // onClick: this.onMenuClick.bind(this)
-        },]
-    }
-
-    render() {
-        return (
-            <FNav
-                uxpId="Nav1"
-                onLinkClick={_onLinkClick}
-                selectedKey={this.props.selectedIndex}
-                expandButtonAriaLabel="Expand or collapse"
-                selectedAriaLabel="Selected"
-                styles={styles}
-                groups={this.getGroups()} />
-        )
-    }
-}
-
-Nav.propTypes = {
-    /** Max width for menu */
-    width: PropTypes.number,
-
-    /** Which element number should be selected from 1 to n */
-    selectedIndex: PropTypes.number,
-};
-
-Nav.defaultProps = {
-    width: 300,
-    selectedIndex: 1
-};
-
-
-export { Nav as default };
+export default (
+    <Nav
+        uxpId="nav1"
+    />
+);
