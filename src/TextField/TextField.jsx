2af2b068a8d48d0df73c3c4f3f0a5ef0f38808c6
Stan Carrico <scarrico@paypal.com>
2019-09-12 12:26:38 -0700

    bug(textfield): container wrapper thing breaks uxp

--- src/TextField/TextField.jsx
+++ src/TextField/TextField.jsx
@@ -2,10 +2,6 @@ import { TextField as FTextField } from 'office-ui-fabric-react';
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
 
-/**
- * @uxpinwrappers
- * SkipContainerWrapper
- */
 function TextField(props) {
   return (
     <FTextField {...props} styles={ {root: {width: props.inputWidth}} } iconProps={{ iconName: props.icon }} ref={props.uxpinRef}/>
