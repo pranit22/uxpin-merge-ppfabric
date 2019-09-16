14eb66a43dc3767869a4861f99cda46c0f4b0857
Savinskiy, Anton <asavinskiy@paypal.com>
2019-09-16 13:52:59 -0700

    feat(Nav): adding front icons

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import { Nav as FNav } from 'office-ui-fabric-react';
-import { name2key } from '../_helpers/parser.js'
+import { name2key, getTokens } from '../_helpers/parser.js'
 import parse from 'csv-parse'
 
 
@@ -30,18 +30,27 @@ class Nav extends React.Component {
         }
     }
 
+    getLeftIcon(str) {
+        if (!getTokens(str).tokens) return null
+        if (!getTokens(str).tokens.find(t => t.type === 'icon')) return null
+        if (getTokens(str).tokens.find(t => t.type === 'icon').position.label !== 'start') return null
+        return getTokens(str).tokens.find(t => t.type === 'icon').target
+    }
+
     setItems(callback) {
         parse(this.props.items, {
             skip_empty_lines: true
         },
             (err, data) => {
+
                 this.setState({
                     links: data
                         .flat()
                         .map((val, i) => ({
-                            name: val,
+                            name: getTokens(val).text,
                             key: name2key(val),
-                            disabled: this.state.disabledIndexes.includes(i + 1)
+                            disabled: this.state.disabledIndexes.includes(i + 1),
+                            icon: this.getLeftIcon(val)
                         }))
                 }, callback)
             })
@@ -64,7 +73,7 @@ class Nav extends React.Component {
         this.setState({
             selectedIndex: index
         }, () => {
-            eval(`this.props.onLinkClick${index}()`)
+            if (eval(`this.props.onLink${index}Click`)) eval(`this.props.onLink${index}Click()`)
         })
     }
 
@@ -107,13 +116,21 @@ Nav.propTypes = {
     disabled: PropTypes.string,
 
     /** clicked element */
-    // onLinkClick: PropTypes.func,
-
-    onLinkClick1: PropTypes.func,
-
-    onLinkClick2: PropTypes.func,
-
-    onLinkClick3: PropTypes.func,
+    onLink1Click: PropTypes.func,
+    onLink2Click: PropTypes.func,
+    onLink3Click: PropTypes.func,
+    onLink4Click: PropTypes.func,
+    onLink5Click: PropTypes.func,
+    onLink6Click: PropTypes.func,
+    onLink7Click: PropTypes.func,
+    onLink8Click: PropTypes.func,
+    onLink9Click: PropTypes.func,
+    onLink10Click: PropTypes.func,
+    onLink11Click: PropTypes.func,
+    onLink12Click: PropTypes.func,
+    onLink13Click: PropTypes.func,
+    onLink14Click: PropTypes.func,
+    onLink15Click: PropTypes.func,
 };
 
 Nav.defaultProps = {
--- src/_helpers/parser.js
+++ src/_helpers/parser.js
@@ -1,3 +1,59 @@
+export const name2key = (str) => {
+  return str
+    .trim()
+    .replace(/[^\w]|_/g, '_')
+    .toLowerCase()
+}
+
+// type(target|mutation1, mutation2)
+export const getTokens = inputStr => {
+  let str = inputStr.trim()
+
+  let reg = new RegExp(/(icon|link)\((.*?)\)/gi)
+  let ts = str.match(reg)
+
+  if (!ts) return { text: str }
+
+  let tokens = []
+  ts.forEach((t, i) => {
+    // token position
+    let position = {}
+    position.start = str.indexOf(t)
+    position.end = str.indexOf(t) + t.length
+    if (str.indexOf(t) === 0) position.label = 'start'
+    if (str.indexOf(t) + t.length === str.length) position.label = 'end'
+    if (!position.label) position.label = 'middle'
+
+    // token type
+    let type = t.slice(0, t.indexOf('('))
+
+    // token target
+    let target = t.match(/(?<=\().*?(?=(\)|\|))/g)[0]
+
+    // token mutators
+    let mutators = t.match(/(?<=\|).*?(?=\))/g)
+    if (mutators && mutators.length > 0) mutators = mutators[0].split(',').map(v => v.trim())
+
+    tokens.push({
+      tokenString: t,
+      type,
+      position,
+      target,
+      mutators
+    })
+  })
+
+
+  // removing tokens from string
+  let cleanStrRegex = new RegExp(tokens
+    .map(tObj => tObj.tokenString.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'))
+    .join('|')
+    , 'gi')
+  let text = str.replace(cleanStrRegex, '')
+
+
+  return { text, tokens }
+}
 
 // export const csv2arr = (str) => {
 //   return str
@@ -12,10 +68,4 @@
 // }
 
 
-export const name2key = (str) => {
-  return str
-    .trim()
-    .replace(/[^\w]|_/g, '_')
-    .toLowerCase()
-}
 
