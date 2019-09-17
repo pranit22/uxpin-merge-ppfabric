21d189c172652f86c11e4ed37abc56646e78a1f1
Savinskiy, Anton <asavinskiy@paypal.com>
2019-09-17 10:05:03 -0700

    chore(Card): basic text feature

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,24 +1,29 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
-import { Card as FCard } from 'office-ui-fabric-react';
+import { Card as FCard } from '@uifabric/react-cards';
+import { Text } from 'office-ui-fabric-react';
 // import parse from 'csv-parse'
 // import { name2key, getTokens } from '../_helpers/parser.js'
 
 
 class Card extends React.Component {
     render() {
-        let params = {
-        }
         return (
-            <FCard {...params}></FCard >
+            <FCard >
+                <FCard.Item>
+                    <Text>{this.props.text}</Text>
+                </FCard.Item>
+            </FCard >
         )
     }
 }
 
 Card.propTypes = {
+    text: PropTypes.string,
 };
 
 Card.defaultProps = {
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mollis sed dolor et dapibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec vehicula eros tortor, sed sagittis dolor congue eu. Nunc suscipit porta sapien. In venenatis diam sed dolor mollis sodales. '
 }
 
 export { Card as default };
