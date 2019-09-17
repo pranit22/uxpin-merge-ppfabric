d863cdf0a1196fef801542220cf232ce029e099a
Savinskiy, Anton <asavinskiy@paypal.com>
2019-09-17 09:44:05 -0700

    chore(Facepile): adjusting available number for facepile

--- src/Facepile/Facepile.jsx
+++ src/Facepile/Facepile.jsx
@@ -2,13 +2,14 @@ import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import { Facepile as FFacepile, PersonaSize } from 'office-ui-fabric-react';
 import { facepilePersonas } from '@uifabric/example-data';
-
-import parse from 'csv-parse'
-import { name2key, getTokens } from '../_helpers/parser.js'
+// import parse from 'csv-parse'
+// import { name2key, getTokens } from '../_helpers/parser.js'
 
 
 class Facepile extends React.Component {
     render() {
+        debugger
+
         let params = {
             personaSize: PersonaSize[this.props.size],
             personas: facepilePersonas.slice(0, this.props.number),
@@ -21,7 +22,7 @@ class Facepile extends React.Component {
 
 Facepile.propTypes = {
     size: PropTypes.oneOf(['size16', 'size24', 'size28', 'size32', 'size40']),
-    number: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+    number: PropTypes.oneOf([1, 2, 3, 4, 5]),
 };
 
 Facepile.defaultProps = {
