572c41bafbcc2ec80e7f1a25e16326ee51d45bc6
Savinskiy, Anton <asavinskiy@paypal.com>
2019-09-26 16:12:13 -0700

    feat(Coachmark): bringing initial structure

--- src/Coachmark/Coachmark.jsx
+++ src/Coachmark/Coachmark.jsx
@@ -1,15 +1,58 @@
 import * as React from 'react';
-import {Coachmark as FCoachmark} from 'office-ui-fabric-react';
+import { Coachmark as FCoachmark, TeachingBubbleContent, DirectionalHint } from 'office-ui-fabric-react';
 import * as PropTypes from 'prop-types';
+import './index.scss';
 
-function Coachmark(props) {
-   return (
-      <FCoachmark {...props}>{props.children}</FCoachmark>
-  );
+class Coachmark extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: this.props.open || false
+        }
+        this._targetElm = React.createRef();
+    }
+    render() {
+        let option = { key: 'A', text: 'Top Left Edge', data: DirectionalHint.topRightEdge }
+        return (
+            <div className="CoachmarkComponent" >
+                <div
+                    className="trigger"
+                    onClick={() => { this.setState({ open: !this.state.open }) }}
+                    ref={this._targetElm} />
+                <FCoachmark
+                    target={this._targetElm.current}
+                    positioningContainerProps={{
+                        directionalHint: {
+                            coachmarkPosition: option.data,
+                            dropdownSelectedOptionKey: option.key
+                        },
+                        // doNotLayer: false
+                    }}>
+                    <TeachingBubbleContent
+                        headline="Example Title"
+                        hasCloseIcon={true}
+                        closeButtonAriaLabel="Close"
+                        primaryButtonProps={{ text: 'Try it' }}
+                        secondaryButtonProps={{ text: 'Try it again' }}
+                        onDismiss={this.props.dismiss}
+                        ariaDescribedBy={'example-description1'}
+                        ariaLabelledBy={'example-label1'} >
+                        Welcome to the land of Coachmarks!
+                    </TeachingBubbleContent>
+                </FCoachmark>
+            </div >
+        );
+    }
 }
 
 Coachmark.propTypes = {
-    children: PropTypes.node,
+    open: PropTypes.bool,
+    dismiss: PropTypes.func,
+
 };
+
+Coachmark.defaultProps = {
+    open: true
+}
 
 export { Coachmark as default };
