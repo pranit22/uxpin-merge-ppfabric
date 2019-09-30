6b5c6c1cb64d574dfb0e6161e70ca257166667a4
Savinskiy, Anton <asavinskiy@paypal.com>
2019-09-30 14:06:18 -0700

    refactor(DetailsList): switching columns to CSV

--- src/DetailsList/DetailsList.jsx
+++ src/DetailsList/DetailsList.jsx
@@ -3,6 +3,8 @@ import * as PropTypes from 'prop-types';
 import { DetailsList as FDetailsList, SelectionMode } from 'office-ui-fabric-react/lib/DetailsList';
 import { Icon } from 'office-ui-fabric-react/lib/Icon';
 import { mergeStyleSets, mergeStyles } from 'office-ui-fabric-react/lib/Styling';
+import parse from 'csv-parse'
+import { name2key, getTokens } from '../_helpers/parser.jsx'
 
 
 
@@ -19,88 +21,123 @@ const classNames = mergeStyleSets({
 class DetailsList extends React.Component {
   constructor(props) {
     super(props);
-  }
 
-  getToken(str) {
-    const lBracket = str.indexOf('[')
-    const rBracket = str.indexOf(']')
-    let [token, value] = [null, str];
-    if (lBracket !== -1) {
-      token = str.substring(lBracket + 1, rBracket)
-      value = lBracket === 0 ? str.substring(rBracket + 1, str.length) : str.substring(0, lBracket)
-    }
-    return [value.trim(), token];
-  }
-
-  tokenToSymbol(token) {
-    if (!token) return null
-    let tokenType = token.split(':')[0]
-    switch (tokenType) {
-      case 'color':
-        return mergeStyles({
-          color: `var(--color-${token.split(':')[1]})`
-        })
-        break;
-      case 'icon':
-        let shape = token.split(':')[1]
-        let color = token.split(':')[2]
-        return <Icon key="icon" className={mergeStyles({
-          fontSize: 16,
-          marginRight: 7,
-          transform: 'translateY(3px)',
-          color: `var(--color-${color})`
-        })} iconName={shape} />
-      default:
-        console.warn(`Unknown token type:${tokenType} was passed to DetailsList`)
+    this.state = {
+      columns: [],
+      items: []
     }
   }
 
-  getColumns() {
-    return this.props.columns
-      .split('\n')
-      .join('|')
-      .split('|')
-      .map(col => col.trim())
-      .map((col, i) => {
-        let [columnName, token] = this.getToken(col);
-        return {
-          key: columnName.toLowerCase(),
-          name: columnName,
-          fieldName: columnName.toLowerCase(),
-          isResizable: true,
-          minWidth: this.props.minWidth,
-          maxWidth: this.props.maxWidth,
-          onColumnClick: () => columnName,
-          headerClassName: classNames.headerCell,
-          className: this.tokenToSymbol(token)
-        }
+  componentDidMount() {
+    this.setColumns()
+  }
+
+  setColumns(callback) {
+    parse(this.props.columns, {
+      skip_empty_lines: true
+    },
+      (err, data) => {
+        this.setState({
+          columns: data
+            .flat()
+            .map((columnName, i) => {
+              console.log(columnName);
+              return {
+                key: columnName,
+                name: columnName,
+                fieldName: columnName,
+                isResizable: true,
+                minWidth: this.props.minWidth,
+                maxWidth: this.props.maxWidth,
+                onColumnClick: () => columnName,
+                headerClassName: classNames.headerCell,
+                // className: this.tokenToSymbol(token)
+              }
+            })
+        }, callback)
       })
   }
 
-  getItems() {
-    return this.props.items
-      .split('\n')
-      .join('||')
-      .split('||')
-      .map(row => (row.split('|').map(val => val.trim())))
-      .map((row, rowInd) => {
-        let r = {
-          key: rowInd,
-        }
-        this.getColumns().forEach((column, colInd) => {
-          let value = row[colInd]
-          let [cellValue, token] = this.getToken(value);
-          r[column.fieldName] = token ? [this.tokenToSymbol(token), cellValue] : value;
-        })
-        return r
-      })
-  }
+  // getToken(str) {
+  //   const lBracket = str.indexOf('[')
+  //   const rBracket = str.indexOf(']')
+  //   let [token, value] = [null, str];
+  //   if (lBracket !== -1) {
+  //     token = str.substring(lBracket + 1, rBracket)
+  //     value = lBracket === 0 ? str.substring(rBracket + 1, str.length) : str.substring(0, lBracket)
+  //   }
+  //   return [value.trim(), token];
+  // }
+
+  // tokenToSymbol(token) {
+  //   if (!token) return null
+  //   let tokenType = token.split(':')[0]
+  //   switch (tokenType) {
+  //     case 'color':
+  //       return mergeStyles({
+  //         color: `var(--color-${token.split(':')[1]})`
+  //       })
+  //       break;
+  //     case 'icon':
+  //       let shape = token.split(':')[1]
+  //       let color = token.split(':')[2]
+  //       return <Icon key="icon" className={mergeStyles({
+  //         fontSize: 16,
+  //         marginRight: 7,
+  //         transform: 'translateY(3px)',
+  //         color: `var(--color-${color})`
+  //       })} iconName={shape} />
+  //     default:
+  //       console.warn(`Unknown token type:${tokenType} was passed to DetailsList`)
+  //   }
+  // }
+
+  // getColumns() {
+  //   return this.props.columns
+  //     .split('\n')
+  //     .join('|')
+  //     .split('|')
+  //     .map(col => col.trim())
+  //     .map((col, i) => {
+  //       let [columnName, token] = this.getToken(col);
+  //       return {
+  //         key: columnName.toLowerCase(),
+  //         name: columnName,
+  //         fieldName: columnName.toLowerCase(),
+  //         isResizable: true,
+  //         minWidth: this.props.minWidth,
+  //         maxWidth: this.props.maxWidth,
+  //         onColumnClick: () => columnName,
+  //         headerClassName: classNames.headerCell,
+  //         className: this.tokenToSymbol(token)
+  //       }
+  //     })
+  // }
+
+  // getItems() {
+  //   return this.props.items
+  //     .split('\n')
+  //     .join('||')
+  //     .split('||')
+  //     .map(row => (row.split('|').map(val => val.trim())))
+  //     .map((row, rowInd) => {
+  //       let r = {
+  //         key: rowInd,
+  //       }
+  //       this.getColumns().forEach((column, colInd) => {
+  //         let value = row[colInd]
+  //         let [cellValue, token] = this.getToken(value);
+  //         r[column.fieldName] = token ? [this.tokenToSymbol(token), cellValue] : value;
+  //       })
+  //       return r
+  //     })
+  // }
 
   render() {
     return (
       <FDetailsList {...this.props}
-        columns={this.getColumns()}
-        items={this.getItems()}
+        columns={this.state.columns}
+        items={this.state.items}
         selectionMode={this.props.selectable ? SelectionMode.multiple : SelectionMode.none}
         onRenderRow={(props, defaultRender) => (
           <>
@@ -150,10 +187,10 @@ DetailsList.propTypes = {
 };
 
 DetailsList.defaultProps = {
-  columns: "Aa | Bb | Cc [color:blue-600]",
-  items: `A-1  | B-1  | C-1
-                A-2 | B-2 | C-2
-                A-3 | B-3 | C-3`,
+  columns: "Aa, Bb, link(C)",
+  items: `A-1, B-1, C-1
+ A-2, B-2, C-2
+ A-3, B-3, C-3`,
   selectable: false,
   isResizable: true,
   minWidth: 100,
--- src/DetailsList/presets/0-default.jsx
+++ src/DetailsList/presets/0-default.jsx
@@ -4,15 +4,11 @@ import DetailsList from '../DetailsList';
 export default (
   <DetailsList
     uxpId="DetailsList1"
-    columns={
-      `Aa
-Bb [color:red-600]
-Cc [color:blue-600]`
-    }
+    columns={`Aa, Bb, link(C)`}
     items={
-      `A-1  | B-1  | C-1 [icon:StatusErrorFull:red-600]
-A-2 | B-2 | C-2 [icon:SkypeCircleCheck:green-600]
-A-3 | B-3 | C-3 [icon:AlertSolid:orange-600]`
+      `A-1, B-1, C-1
+ A-2, B-2, C-2
+ A-3, B-3, C-3`
     }
     isResizable />
 )
