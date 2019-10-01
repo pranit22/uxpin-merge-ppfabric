af0fae132bee3e09010e81c6e16d6698f8f694bd
Savinskiy, Anton <asavinskiy@paypal.com>
2019-10-01 13:28:20 -0700

    chore(DetailsList): adjusting example for column icon

--- src/DetailsList/presets/0-default.jsx
+++ src/DetailsList/presets/0-default.jsx
@@ -4,7 +4,7 @@ import DetailsList from '../DetailsList';
 export default (
   <DetailsList
     uxpId="DetailsList1"
-    columns={`Aa icon(CompassNW),  Bb, Cc link(link)`}
+    columns={`Aa icon(StockUp|color-blue-500),  Bb, Cc link(link)`}
     items={
       `A-1, B-1, C-1 icon(SkypeCircleCheck|color-green-700)
  A-2, B-2,  icon(SkypeCircleMinus|color-red-700) C-2 icon(SkypeCircleSlash|color-blue-700)  icon(SkypeCircleArrow|color-orange-700)
