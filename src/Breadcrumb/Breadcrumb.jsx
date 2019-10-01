abf2e8a93bc48bbf3df5d660666f59ee66119e7f
Savinskiy, Anton <asavinskiy@paypal.com>
2019-10-01 15:34:47 -0700

    chore(BreadCrumb): adjusting

--- src/Breadcrumb/Breadcrumb.jsx
+++ src/Breadcrumb/Breadcrumb.jsx
@@ -1,31 +1,48 @@
 import { Breadcrumb as FBreadcrumb, FontSizes } from 'office-ui-fabric-react';
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
-import "./index.scss";
+import { mergeStyles } from '@uifabric/merge-styles';
+
+
 
 function Breadcrumb(props) {
-  return (
-    <FBreadcrumb className="BreadcrumbComponent" items={props.crumbs.split(',').map(text => ({text}))} styles={{
-      itemLink : {
-        fontSize: FontSizes.medium
+  let breadcrumbClasses = mergeStyles({
+    marginTop: -5,
+    marginBottom: -10,
+    display: 'inline - block',
+    width: 'min-content',
+    height: 'min-content',
+    selectors: {
+      '& .ms-Breadcrumb-item': {
+        fontSize: FontSizes[props.fontSize],
       },
-      item : {
-        fontSize: FontSizes.medium
+      '& .ms-Breadcrumb-list': {
+        height: 31,
+        padding: '15px 0px 20px 0px',
+        verticalAlign: 'middle'
       }
-    }} {...props} />
+    }
+  })
+
+  return (
+    <FBreadcrumb className="BreadcrumbComponent"
+      items={props.crumbs.split(',').map(text => ({ text }))}
+      className={breadcrumbClasses} {...props} />
   );
 }
 
 Breadcrumb.propTypes = {
   crumbs: PropTypes.string.isRequired,
   maxDisplayedItems: PropTypes.number,
-  overflowIndex: PropTypes.number
+  overflowIndex: PropTypes.number,
+  size: PropTypes.oneOf(Object.keys(FontSizes)),
 };
 
 Breadcrumb.defaultProps = {
   crumbs: 'foo,bar,baz',
   maxDisplayedItems: 3,
-  overflowIndex: 0
+  overflowIndex: 0,
+  fontSize: 'small'
 };
 
 export { Breadcrumb as default };
