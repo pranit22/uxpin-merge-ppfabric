cf0e06ba3c6e19f5291e2c09225d7c00766bd4e8
Pranit Mhatre <pramhatre@paypal.com>
2020-06-10 14:57:19 -0700

    feat(ppcard): adding shimmer component

--- src/PPCard/PPCard.jsx
+++ src/PPCard/PPCard.jsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import {
-        Stack,
-        StackItem,
-        Text 
-    } from 'office-ui-fabric-react';
+    Stack,
+    StackItem,
+    Text,
+    Shimmer,
+    ShimmerElementType
+} from 'office-ui-fabric-react';
+import PPVerticalStack from '../PPVerticalStack/PPVerticalStack';
 import * as PropTypes from 'prop-types';
 import { TpxUxColors } from '../_helpers/tpxuxcolorutils.jsx';
 import _ from 'lodash';
@@ -20,7 +23,7 @@ const verticalAlign = 'center';
 const leftAlign = 'left';
 const centerAlign = 'center';
 const rightAlign = 'right';
-    
+
 //****** STYLES */
 
 //Use this color if the UXPin user doesn't enter a valid hex or PPUI color token.
@@ -29,8 +32,10 @@ const defaultTextColor = "#000000";
 const borderRadius = '4px';
 
 //PPUI Elevation Specs: https://engineering.paypalcorp.com/confluence/display/PPUI/Elevation
-const elevationShadow0 = '0 2px 4px rgba(0, 0, 0, 0.16)';  
+const elevationShadow0 = '0 2px 4px rgba(0, 0, 0, 0.16)';
 const elevationShadow1 = '0 3px 10px rgba(0, 0, 0, 0.16)';
+
+const defaultShimmerDuration = 1;
 
 //****** OTHER */
 
@@ -47,6 +52,36 @@ class PPCard extends React.Component {
 
         this.state = {
             hovered: false,
+            shimmer: true
+        }
+    }
+
+    componentDidMount() {
+        this.set();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (
+            prevProps.shimmer !== this.props.shimmer ||
+            prevProps.shimmerDuration !== this.props.shimmerDuration
+        ) {
+            this.set();
+        }
+    }
+
+    set() {
+        this.setState({
+            shimmer: this.props.shimmer
+        })
+        if (this.props.shimmer) {
+            setTimeout(
+                () => {
+                    this.setState({
+                        shimmer: false
+                    })
+                },
+                (this.props.shimmerDuration || defaultShimmerDuration) * 1000
+            )
         }
     }
 
@@ -56,13 +91,13 @@ class PPCard extends React.Component {
 
     _getHorizontalAlignmentToken() {
         switch (this.props.align) {
-            case leftAlign :
+            case leftAlign:
                 return 'start';
-            case centerAlign :
+            case centerAlign:
                 return 'center';
-            case rightAlign :
+            case rightAlign:
                 return 'end';
-            default :
+            default:
                 return 'start';
         }
     }
@@ -70,7 +105,7 @@ class PPCard extends React.Component {
     render() {
 
         //An empty string will cause the Text control to hide.
-        let instructions = this.props.showInstructions ? this.props.value : '' ;
+        let instructions = this.props.showInstructions ? this.props.value : '';
 
         let hAlign = this._getHorizontalAlignmentToken();
 
@@ -81,8 +116,8 @@ class PPCard extends React.Component {
 
         const topStackItemStyles = {
             root: {
-              display: 'flex',
-              overflow: 'hidden',
+                display: 'flex',
+                overflow: 'hidden',
             },
         };
 
@@ -92,13 +127,13 @@ class PPCard extends React.Component {
         //Let's see if the user entered a valid color value. This method returns undefined if not. 
         let cardBgColor = TpxUxColors.getHexFromHexOrPpuiToken(this.props.bgColor);
 
-        let cardShadow =   this.state.hovered ? elevationShadow1 : elevationShadow0; 
+        let cardShadow = this.state.hovered ? elevationShadow1 : elevationShadow0;
 
         const divStyles = {
-              backgroundColor: cardBgColor,
-              borderRadius: borderRadius,
-              boxShadow: this.props.showShadow ? cardShadow : '',
-              padding: cardPad + 'px',
+            backgroundColor: cardBgColor,
+            borderRadius: borderRadius,
+            boxShadow: this.props.showShadow ? cardShadow : '',
+            padding: cardPad + 'px',
         };
 
         //With one number, the padding applies to both rows and columns.  
@@ -107,7 +142,7 @@ class PPCard extends React.Component {
 
         const stackTokens = {
             childrenGap: pad,
-            padding: 0, 
+            padding: 0,
         };
 
         //****************************
@@ -122,7 +157,7 @@ class PPCard extends React.Component {
             }
         }
 
-        
+
         //****************************
         //For Inner Stack
 
@@ -137,12 +172,12 @@ class PPCard extends React.Component {
             if (childList.length) {
                 for (var i = 0; i < childList.length; i++) {
                     if (childList[i]) {
-                        let child = _.cloneDeep(childList[i]);   
-                        child.cardTheme = this.props.cardTheme;   
+                        let child = _.cloneDeep(childList[i]);
+                        child.cardTheme = this.props.cardTheme;
                         let stack = (
-                            <StackItem 
-                                align = { 'stretch' }   >
-                                { child }
+                            <StackItem
+                                align={'stretch'}   >
+                                {child}
                             </StackItem>
                         );
                         stackList.push(stack);
@@ -153,30 +188,48 @@ class PPCard extends React.Component {
 
 
         return (
-            <div 
-                    {...this.props} 
-                    style= { divStyles } 
-                    onMouseEnter = {() => this._setHover(true)}
-                    onMouseLeave = {() => this._setHover(false)} >
-
-            <Stack 
+            <div
                 {...this.props}
-                tokens = { stackTokens }
-                horizontal = { false }
-                horizontalAlign = { hAlign }
-                verticalAlign = { verticalAlign }
-                wrap = { false }
-                styles = { topStackItemStyles }> 
-                    <Text
+                style={divStyles}
+                onMouseEnter={() => this._setHover(true)}
+                onMouseLeave={() => this._setHover(false)} >
+                {this.state.shimmer && (
+                    <PPVerticalStack
+                        showInstructions={false}
+                        gutterPadding={10}
+                    >
+                        <Shimmer shimmerElements={[
+                            { type: ShimmerElementType.line, height: 24 },
+                        ]} />
+                        <Shimmer shimmerElements={[
+                            { type: ShimmerElementType.line, height: 50 }
+                        ]} />
+                        <Shimmer shimmerElements={[
+                            { type: ShimmerElementType.line, height: 24, },
+                        ]} />
+                    </PPVerticalStack>
+                )}
+                {!this.state.shimmer && (
+                    <Stack
                         {...this.props}
-                        styles = { fTextStyles }
-                        variant = { 'medium' }>
-                        { instructions }
-                    </Text>
+                        tokens={stackTokens}
+                        horizontal={false}
+                        horizontalAlign={hAlign}
+                        verticalAlign={verticalAlign}
+                        wrap={false}
+                        styles={topStackItemStyles}>
+                        <React.Fragment>
+                            <Text
+                                {...this.props}
+                                styles={fTextStyles}
+                                variant={'medium'}>
+                                {instructions}
+                            </Text>
 
-                    { stackList }
-
-            </Stack>
+                            {stackList}
+                        </React.Fragment>
+                    </Stack>
+                )}
             </div>
         );
     }
@@ -194,7 +247,7 @@ PPCard.propTypes = {
      * @uxpinignoreprop 
      * @uxpindescription Contents for the right side. 1. Drag an object onto the canvas. 2. In the Layers Panel, drag the item onto this object. Now it should be indented, and contained as a 'child.'  
      * @uxpinpropname Right Contents
-     */ 
+     */
     children: PropTypes.node,
 
     /**
@@ -209,40 +262,52 @@ PPCard.propTypes = {
     /**
      * @uxpindescription To show or hide the instructional text  
      * @uxpinpropname Show Instructions
-     */ 
-    showInstructions: PropTypes.bool, 
+     */
+    showInstructions: PropTypes.bool,
 
     /**
      * NOTE: This cannot be called just 'padding,' or else there is a namespace collision with regular CSS 'padding.'
      * @uxpindescription Inner padding for all card contents. Value must be 0 or more.  
      * @uxpinpropname Card Padding
-     */ 
-    cardPadding: PropTypes.number,  
+     */
+    cardPadding: PropTypes.number,
 
     /**
      * NOTE: This cannot be called just 'padding,' or else there is a namespace collision with regular CSS 'padding.'
      * @uxpindescription Padding between the sections in the card. Value must be 0 or more.  
      * @uxpinpropname Section Padding
-     */ 
-    gutterPadding: PropTypes.number,  
-    
+     */
+    gutterPadding: PropTypes.number,
+
     /**
      * @uxpindescription To horizontally align all content within the stack 
      * @uxpinpropname Alignment
-     */    
+     */
     align: PropTypes.oneOf([leftAlign, centerAlign, rightAlign]),
 
     /**
      * @uxpindescription Use a PayPal UI color token, such as 'blue-600' or 'black', or a standard Hex Color, such as '#0070BA'
      * @uxpinpropname Bg Color
-     * */  
+     * */
     bgColor: PropTypes.string,
 
     /**
      * @uxpindescription To show or hide the card's background shadow  
      * @uxpinpropname Show Shadow
-     */ 
-    showShadow: PropTypes.bool, 
+     */
+    showShadow: PropTypes.bool,
+
+    /**
+    * @uxpindescription Whether to display the shimmer 
+    * @uxpinpropname Shimmer
+    */
+    shimmer: PropTypes.bool,
+
+    /**
+    * @uxpindescription Shimmer duration inseconds
+    * @uxpinpropname Shimmer Duration
+    */
+    shimmerDuration: PropTypes.number,
 }
 
 
@@ -250,13 +315,15 @@ PPCard.propTypes = {
  * Set the default values for this control in the UXPin Editor.
  */
 PPCard.defaultProps = {
-    value: instructionText,    
+    value: instructionText,
     showInstructions: true,
     cardPadding: 12,
     gutterPadding: 12,
     align: leftAlign,
     bgColor: '',
     showShadow: true,
+    shimmer: true,
+    shimmerDuration: defaultShimmerDuration
 }
 
 
