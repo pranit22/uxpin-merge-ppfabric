88b135562d538fa2e032c34b2bac5b91d36a340c
Pranit Mhatre <pramhatre@paypal.com>
2020-06-23 17:37:57 -0700

    feat(tooltips): adding tooltips to buttons

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,23 +1,22 @@
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
-import { mergeStyles, mergeStyleSets } from '@uifabric/merge-styles';
-import { PrimaryButton as FPrimaryButton, DefaultButton as FDefaultButton, IButtonStyles } from 'office-ui-fabric-react';
+import { PrimaryButton as FPrimaryButton, DefaultButton as FDefaultButton, TooltipHost } from 'office-ui-fabric-react';
 
 
-  /**
-   * UPDATED Mar 16-17, 2020 by Anthony Hand
-   * - Removed 'checked' property. This property is not required currently.
-   * - Added descriptions and prop names for each property with some updates. 
-   * - Removed the 'onClick' prop assignment from the <FButton>attributes in the Return method. They were unnecessary.
-   * 
-   * TODOs
-   * - Allow for user resizing of the width. 
-   * - Ensure all styling is as per PPUI spec.
-   * 
-   * For additional outstanding issues, please see: 
-   *    https://github.paypal.com/Console-R/uxpin-merge-ms-fabric/issues/48
-   *    
-   * */
+/**
+ * UPDATED Mar 16-17, 2020 by Anthony Hand
+ * - Removed 'checked' property. This property is not required currently.
+ * - Added descriptions and prop names for each property with some updates. 
+ * - Removed the 'onClick' prop assignment from the <FButton>attributes in the Return method. They were unnecessary.
+ * 
+ * TODOs
+ * - Allow for user resizing of the width. 
+ * - Ensure all styling is as per PPUI spec.
+ * 
+ * For additional outstanding issues, please see: 
+ *    https://github.paypal.com/Console-R/uxpin-merge-ms-fabric/issues/48
+ *    
+ * */
 
 
 class Button extends React.Component {
@@ -40,20 +39,30 @@ class Button extends React.Component {
       flexDirection: 'row-reverse'
     }
 
+    const tooltipId = _.uniqueId('tooltip_');
+
     return (
 
-      <div>
+      <TooltipHost
+        content={this.props.tooltip}
+        id={tooltipId}
+      >
         {this.props.primary ?
-          <FPrimaryButton  {...this.props}
-            iconProps={iconProps}
-            styles={styles} />
-          :
-          <FDefaultButton {...this.props}
+          <FPrimaryButton
+            {...this.props}
             iconProps={iconProps}
             styles={styles}
+            aria-describedby={tooltipId}
+          />
+          :
+          <FDefaultButton
+            {...this.props}
+            iconProps={iconProps}
+            styles={styles}
+            aria-describedby={tooltipId}
           />
         }
-      </div>
+      </TooltipHost>
 
     );
   }
@@ -81,31 +90,37 @@ Button.propTypes = {
   /**
    * @uxpindescription The exact name from the PayPal icon library (Optional)
    * @uxpinpropname Icon Name
-   * */  
+   * */
   iconName: PropTypes.string,
 
   /**
    * @uxpindescription To disable the control
    * @uxpinpropname Disabled
-   * */  
+   * */
   disabled: PropTypes.bool,
 
   /**
    * @uxpindescription The location to display an icon, if one is set
    * @uxpinpropname Icon Position
-   * */   
+   * */
   iconPosition: PropTypes.oneOf(['start', 'end']),
 
   /**
    * @uxpindescription Sets whether to display the button in the rounded PayPal UI style.
    * @uxpinpropname Rounded
-   * */   
+   * */
   rounded: PropTypes.bool,
+
+  /**
+   * @uxpindescription Tooltip for the control
+   * @uxpinpropname Tooltip
+   * */
+  tooltip: PropTypes.string,
 
   /**
    * @uxpindescription Fires when the button is clicked on.
    * @uxpinpropname Click
-   * */   
+   * */
   onClick: PropTypes.func
 };
 
@@ -117,7 +132,8 @@ Button.defaultProps = {
   primary: true,
   disabled: false,
   rounded: true,
-  text: "Basic Button"
+  text: "Basic Button",
+  tooltip: ''
 };
 
 
